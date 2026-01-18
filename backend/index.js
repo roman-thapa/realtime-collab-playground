ce3db28a867adcdb77823fcc59150c179af2a570
roman <51534566+roman-thapa@users.noreply.github.com>
2026-01-18 15:53:47 +0530

    feat: handle basic routing for / and /favicon.ico

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const http = require('http')
 const server = http.createServer((request, response) => {
-    console.log(response.prototype)
-     response.write("Request received")
-     response.end()
+    if (request.url === '/favicon.ico') {
+        response.end()
+    } else if (request.url === '/') {
+        response.write("We have received your request and we acknowledge it\n")
+        response.end()
+    }
 })
 
 server.listen(8080, () => (console.log("server is running on port 8080")))
