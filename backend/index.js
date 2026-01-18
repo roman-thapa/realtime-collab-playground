9e0edfb28976f4fa2d9db391650ca8682e2654c2
roman <51534566+roman-thapa@users.noreply.github.com>
2026-01-18 15:12:20 +0530

    feat: respond to HTTP request using raw Node server

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
 const http = require('http')
-const server = http.createServer()
+const server = http.createServer((request, response) => {
+    console.log(response.prototype)
+     response.write("Request received")
+     response.end()
+})
 
-server.listen(8080, console.log("server is running on port 8080"))
+server.listen(8080, () => (console.log("server is running on port 8080")))
