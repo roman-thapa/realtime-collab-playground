94c7b0f05c22cb388874cf993f01e1fc2da17961
roman <51534566+roman-thapa@users.noreply.github.com>
2026-01-26 00:51:40 +0530

    feat: add POST /login endpoint to receive and log JSON data

--- backend/express-server.js
+++ backend/express-server.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
 app.listen(8080, ()=>(console.log("server is running")))
 app.get('/', (req, res) => {
     res.send("We have received your request and we acknowledge it")
+})
+app.post('/login', (req, res) => {
+    console.log(req.body)
+    res.send("we received a login request and we are processing it\n")
 })
